4f148f94182bad44c3b6fbbf665dde2401297147
kovacs3030 <breckstack@gmail.com>
2011-11-25 14:15:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nullverse::Application.config.secret_token = 'd53b90626fd1c5bee1fb24b3f7c35cff6bde237e6cf61e8ff23844643802fe241df54b75a747b4ee89ffc6c8da8d3f55d9f1270698904886a4a0267db6f435ad'
